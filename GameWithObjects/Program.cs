dc8e1c5611a501bc4d124e594706622b0b628245
Jamie Myland <jamie.myland@aru.ac.uk>
2023-10-03 09:47:23 +0100

    Added simple game engine

--- GameWithObjects/Program.cs
+++ GameWithObjects/Program.cs
@@ -2,9 +2,56 @@
 {
     internal class Program
     {
+        static Random random = new Random();
+        static bool isRunning = true;
+
+        static DateTime lastUpdateTime = DateTime.Now;
+        static double deltaTime;
+        static double gameTimeElapsed;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Game with objects");
+            Console.BufferHeight = Console.WindowHeight * 2;
+            Console.CursorVisible = false;
+
+            while (isRunning)
+            {
+                // Update the time
+                DateTime currentTime = DateTime.Now;
+                deltaTime = (currentTime - lastUpdateTime).TotalMilliseconds;
+                lastUpdateTime = currentTime;
+                gameTimeElapsed += deltaTime;
+
+                Update();
+                Draw();
+                Thread.Sleep(100);  // 100 milliseconds delay
+            }
+        }
+
+        static void Update()
+        {
+            // Handle input
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Q:
+                        isRunning = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void Draw()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine($"Time elapsed (ms): {Math.Round(gameTimeElapsed / 1000)}");
         }
     }
+
 }
